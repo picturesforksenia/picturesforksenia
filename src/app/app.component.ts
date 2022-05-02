b06cd1e0bc24a0e1193526128092b97ca3e6d377
Simon Dieckmann <simon.dieckmann@tngtech.com>
2022-05-02 17:20:25 +0200

    memory game first try

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,104 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CardData } from './card-data.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'PicturesForKsenia';
+export class AppComponent implements OnInit {
+
+  cardImages = [
+    'f62f61ae-ebf9-4a7c-a696-dd06765f697f.JPG',
+    '6597e509-f388-460e-8d2e-df1733050eeb.JPG',
+    'd54d701d-665a-4403-8414-98e221f2ff9d.JPG',
+    '8bee9e87-549a-4ee2-bc39-5c0b35c7d4fb',
+    'c408099f-ccae-4e70-84d0-f3533dedb629',
+    '4d7df00e-8d14-4b83-946e-7f4075e20a71.JPG',
+    '3a7e6b13-6ae4-44d4-895d-341e008451aa.JPG',
+    'fce2a2cb-e654-435a-bae1-41cf36c3964b.JPG'
+  ];
+
+  private NUMBER_OF_CARDS: Number = 5;
+
+  cards: CardData[] = [];
+
+  flippedCards: CardData[] = [];
+
+  matchedCount = 0;
+
+  shuffleArray(anArray: any[]): any[] {
+    return anArray.map(a => [Math.random(), a])
+      .sort((a, b) => a[0] - b[0])
+      .map(a => a[1]);
+  }
+
+  constructor(private dialog: MatDialog) {
+
+  }
+
+  ngOnInit(): void {
+    this.setupCards();
+  }
+
+  setupCards(): void {
+    this.cards = [];
+    this.cardImages.filter((value, index) => index < this.NUMBER_OF_CARDS).forEach((image) => {
+      const cardData: CardData = {
+        imageId: image,
+        state: 'default'
+      };
+
+      this.cards.push({ ...cardData });
+      this.cards.push({ ...cardData });
+
+    });
+
+    this.cards = this.shuffleArray(this.cards);
+  }
+
+  cardClicked(index: number): void {
+    const cardInfo = this.cards[index];
+
+    if (cardInfo.state === 'default' && this.flippedCards.length < 2) {
+      cardInfo.state = 'flipped';
+      this.flippedCards.push(cardInfo);
+
+      if (this.flippedCards.length > 1) {
+        this.checkForCardMatch();
+      }
+
+    } else if (cardInfo.state === 'flipped') {
+      cardInfo.state = 'default';
+      this.flippedCards.pop();
+
+    }
+  }
+
+  checkForCardMatch(): void {
+    setTimeout(() => {
+      const cardOne = this.flippedCards[0];
+      const cardTwo = this.flippedCards[1];
+      const nextState = cardOne.imageId === cardTwo.imageId ? 'matched' : 'default';
+      cardOne.state = cardTwo.state = nextState;
+
+      this.flippedCards = [];
+
+      if (nextState === 'matched') {
+        this.matchedCount++;
+
+        if (this.matchedCount === this.NUMBER_OF_CARDS) {
+            this.restart();
+        }
+      }
+
+    }, 1000);
+  }
+
+  restart(): void {
+    this.matchedCount = 0;
+    this.setupCards();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameCardComponent } from './game-card/game-card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameCardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
