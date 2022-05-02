1e3a28156b4bc5303aef7479dacb0e5d4f4cc332
Simon Dieckmann <simon.dieckmann@tngtech.com>
2022-05-02 14:50:45 +0200

    prod build

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'picturesforksenia.github.io'`, () => {
+  it(`should have as title 'PicturesForKsenia'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('picturesforksenia.github.io');
+    expect(app.title).toEqual('PicturesForKsenia');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('picturesforksenia.github.io app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('PicturesForKsenia app is running!');
   });
 });
